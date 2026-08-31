7a15f9b0882c239e45d36bdc1c73ad5f997d15f8
Venkatesh <venkateshnaik.gov@gmail.com>
2026-09-01 02:58:36 +0530

    chore: reset workspace for Java Spring Boot architecture transition

--- backend/src/routes/ehrRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getEHRs, createEHR } from '../controllers/ehrController';
-import { authMiddleware } from '../middlewares/auth';
-
-const router = Router();
-router.use(authMiddleware);
-router.get('/', getEHRs);
-router.post('/', createEHR);
-export default router;
